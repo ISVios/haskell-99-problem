d8b5d4fa46893479c824414c644aaa7bd32ebdd3
Vios <i.svoboda.vios@gmail.com>
2021-08-09 21:28:51 +0300

    Problem 3

--- Solution.hs
+++ Solution.hs
@@ -6,12 +6,18 @@ myLast (x:[]) = x
 myLast (x:xs) = myLast xs
 -- 
 -- Problem 2
-myButLast :: [a] -> [a] 
+myButLast :: [a] -> a 
 myButLast []       = error "Solution.myButLast: list is empty."
 myButLast (x:[])   = error "Solution.myButLast: size of list must be >= 2."
 myButLast (x:_:[]) = x 
 --
---
+-- Problem 3
+elementAt :: [a] -> Int -> a
+elementAt []     _                     = error "Solution.elementAt: list is empty."
+elementAt (x:_)  0                     = x
+elementAt (x:xs) i | (succ i) > length (x:xs) = error "Solution.elementAt: index is great than size of list."
+                   | i < 0                    = error "Solution.elementAt: index must be >= 0."
+                   | otherwise                = elementAt xs (pred i) 
 --
 --
 --
