c1dd302686c6492c2eda424cff8859efd5278c29
Vios <i.svoboda.vios@gmail.com>
2021-08-09 21:45:54 +0300

    Problem 7

--- Solution.hs
+++ Solution.hs
@@ -33,7 +33,13 @@ myReverse (x:xs) = myReverse xs ++ [x]
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = myReverse xs == xs -- or use Prelude.reverse
 --
+-- Problem 7
+data NestedList a = Elem a | List [NestedList a]
 --
+flatten :: NestedList a -> [a]
+flatten (Elem a)     = [a]
+flatten (List [])    = []
+flatten (List(x:xs)) = flatten x ++ flatten (List xs)
 --
 --
 --
